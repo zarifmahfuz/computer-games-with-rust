2f1b87c3a0f6c09a5b7af51de511e2adaa5c6064
Zarif Mahfuz <mahfuz@ualberta.ca>
2022-04-10 16:14:15 -0600

    show computer stats

--- front_end/src/pages/score.rs
+++ front_end/src/pages/score.rs
@@ -21,6 +21,16 @@ struct Winner {
     wins: i64,
 }
 
+#[derive(Clone, PartialEq, Deserialize)]
+struct ComputerStats {
+    hard_played: u64,
+    hard_won: u64,
+    medium_played: u64,
+    medium_won: u64,
+    easy_played: u64,
+    easy_won: u64,
+}
+
 #[derive(Clone, Properties, PartialEq)]
 struct GameResultsProps {
     game_results: Vec<GameResult>,
@@ -56,6 +66,29 @@ fn leaders(LeadersProps { leaders }: &LeadersProps) -> Html {
         <td>{ format!("{}", winner.winner_name) }</td>
         <td>{ format!("{}", winner.wins) }</td>
         </tr>
+    }).collect()
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct CStatsProps {
+    stats: Vec<ComputerStats>,
+}
+
+#[function_component(CStats)]
+fn cstats(CStatsProps { stats }: &CStatsProps) -> Html {
+    stats.iter().map(|stat| {
+        let total_played = stat.hard_played + stat.medium_played + stat.easy_played;
+        html! {
+            <tr>
+            <td>{ format!("{}", total_played) }</td>
+            <td>{ format!("{}", stat.hard_played) }</td>
+            <td>{ format!("{}", stat.hard_won) }</td>
+            <td>{ format!("{}", stat.medium_played) }</td>
+            <td>{ format!("{}", stat.medium_won) }</td>
+            <td>{ format!("{}", stat.easy_played) }</td>
+            <td>{ format!("{}", stat.easy_won) }</td>
+            </tr>
+        }
     }).collect()
 }
 
@@ -151,11 +184,46 @@ fn leaderboard() -> Html {
             || ()
         }, ());
     }
+    let computer_stats = use_state(|| vec![]);
+    {
+        let computer_stats = computer_stats.clone();
+        use_effect_with_deps(move |_| {
+            let computer_stats = computer_stats.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let fetched_results: Vec<ComputerStats> = Request::get("/api/computer_stats")
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+                    computer_stats.set(fetched_results);
+            });
+            || ()
+        }, ());
+    }
     html! {
         <div style="margin-left:30%">
             <div class="w3-container" id="services" style="margin-top:75px">
                 <h5 class="w3-xxxlarge w3-text-red"><b>{"Score Board"}</b></h5>
                 <hr style="width:50px;border:5px solid red" class="w3-round"/>
+
+                <div><h4>{"Games Won by Computer"}</h4></div>
+                    <div id="game-stream">
+                    <table>
+                        <tr>
+                            <th>{"Total Played"}</th>
+                            <th>{"Hard Played"}</th>
+                            <th>{"Hard Won"}</th>
+                            <th>{"Medium Played"}</th>
+                            <th>{"Medium Won"}</th>
+                            <th>{"Easy Played"}</th>
+                            <th>{"Easy Won"}</th>
+                        </tr>
+                        <CStats stats={(*computer_stats).clone()}/>
+                    </table>
+                </div>
+                <br/>
 
                 <div><h4>{"Details of Games Won by Computer"}</h4></div>
                     <div id="game-stream">
@@ -208,7 +276,7 @@ fn leaderboard() -> Html {
                     </table>
                 </div>
                 <br/>
-                <div><h4>{"Details of Games Won by All Players Against in Easy Difficulty"}</h4></div>
+                <div><h4>{"Details of Games Won by All Players in Easy Difficulty"}</h4></div>
                 <div>
                     <table>
                         <tr>
